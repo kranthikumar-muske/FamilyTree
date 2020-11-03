4db81f3074839a83a2156e1d02f53903db0c02a7
Kranthi Muske <kranthikumar.muske@cerner.com>
2020-11-03 17:23:25 -0600

    siteconstruction image

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import Sidebar from './components/sidebar'
-import Introduction from './components/introduction'
-import About from './components/about'
-import Skills from './components/skills'
-import Timeline from './components/timeline'
 
-class App extends Component {
-  render() {
-    return (
-      <div id="colorlib-page">
-        <div id="container-wrap">
-         	<Sidebar></Sidebar>
-				<div id="colorlib-main">
-					<Introduction></Introduction>
-          	</div>
-      	</div>
-      </div>
+const CardList = (props) => (
+	<div>
+  	{props.profiles.map(profile => <Card key={profile.userId} {...profile}/>)}
+	</div>
+);
+
+class Card extends React.Component {
+	render() {
+    const profile = this.props;
+    console.log(profile);
+  	return (
+    	<div className="github-profile">
+        <div className="info">
+          <div className="firstname">{profile.firstName}</div>
+          <div className="lastname">{profile.lastName}</div>
+          <div className="age">{profile.age}</div>
+          <div className="gender">{profile.gender}</div>
+          <div className="dob">{profile.dob}</div>
+          <div className="phonenumber">{profile.phoneNumber}</div>
+          <div className="birthplace">{profile.birthplace}</div>
+          <div className="status">{profile.status}</div>
+        </div>
+    	</div>
     );
   }
+}
+
+
+class Form extends React.Component{
+  state = {userName: ''};
+  handleSubmit = async (event)=>{
+    event.preventDefault();
+    
+    const response = await fetch('https://lf88rlmmd0.execute-api.us-east-2.amazonaws.com/dev/dev?'+ new URLSearchParams({
+      firstname : this.state.userName,
+    }))
+
+    const body = await response.json();
+    const awsresponse = [body];
+    this.props.onSubmit(awsresponse); // set bpdy here
+    this.setState({userName: ''});
+  }
+  render(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <input 
+        type = "text"
+        value = {this.state.userName}
+        onChange = {event=>this.setState({userName: event.target.value})}
+        placeholder = "first name..."/>
+        <button>Search</button>
+      </form>
+    );
+  }
+} 
+
+class App extends React.Component {
+  state = {
+    profiles: [],
+  };
+
+  addNewProfile = (profileData) => {
+    this.setState(prevState=>({
+  profiles: [...profileData],
+  }));
+};
+
+	render() {
+  	return (
+    	<div>
+    	  <div className="header">Search you Family Member by Name Here</div>
+        <Form onSubmit={this.addNewProfile}/>
+        <CardList profiles={this.state.profiles}/>
+        <img style={{display: "flex", height:200, width: 300, right: true, marginRight: "auto"}} className = "siteimage" src="https://www.phoenix.gov/eodsite/SiteAssets/Pages/Site-Under-Construction/website-construction-graphic-4.jpg" alt="new"/>
+    	</div>
+    );
+  }	
 }
 
 export default App;
